1942746066f0a031cc20eeb898bd363d0e0680a9
Priyam Shah <priyamshah@Priyams-MacBook-Air.local>
2021-09-22 10:10:46 +0530

    home done with bugs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,65 @@
-import React from "react";
-import { Container, Grow } from "@material-ui/core";
+import React, { Component } from "react";
+import { exportComponentAsPNG } from "react-component-export-image";
+import "./styles.css";
 
-const Home = () => {
-  return (
-    <Grow in>
-      <Container maxwidth="xl">Home</Container>
-    </Grow>
-  );
-};
+class Home extends Component {
+  certificateWrapper = React.createRef();
+  state = {
+    Name: "",
+  };
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <div className="Meta">
+            <p>enter your name.</p>
+            <input
+              type="text"
+              placeholder="name"
+              value={this.state.Name}
+              onChange={(e) => {
+                this.setState({ Name: e.target.value });
+              }}
+            />
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                exportComponentAsPNG(this.certificateWrapper, {
+                  html2CanvasOptions: { backgroundColor: null },
+                });
+              }}
+            >
+              Download
+            </button>
+          </div>
+
+          <div id="downloadWrapper" ref={this.certificateWrapper}>
+            <div id="certificateWrapper">
+              <p>{this.state.Name}</p>
+              <img
+                src="https://imgurupload.org/files/Certi-gen.png"
+                alt="Certificate"
+                width="1024"
+                height="768"
+              />
+            </div>
+          </div>
+        </div>
+        <form>
+          To do
+          <br />
+          <input type="checkbox" name="1"></input>
+          <label for="1">fix download</label>
+          <br />
+          <input type="checkbox" name="2"></input>
+          <label for="1"> implement csv name input</label>
+          <br />
+          <input type="checkbox" name="3"></input>
+          <label for="1">implement certi distribution via name/id input</label>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Home;
